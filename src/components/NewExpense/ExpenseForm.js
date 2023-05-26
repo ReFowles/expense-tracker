5348b0cd6b172b9bb2be28b70a1429eff1118634
ReFowles <rebekah.fowles@gmail.com>
2023-05-26 15:03:56 -0600

    feat: add conditional rendering to the New Expense form
    For Assignment 4.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,9 +3,23 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm(props) {
+  const [isFormDisplayed, setIsFormDisplayed] = useState(false);
+
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
+
+  const formDisplayHandler = (event) => {
+    if (isFormDisplayed === false) {
+      setIsFormDisplayed(true);
+    } else {
+      event.preventDefault();
+      setEnteredTitle("");
+      setEnteredAmount("");
+      setEnteredDate("");
+      setIsFormDisplayed(false);
+    }
+  };
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -27,49 +41,58 @@ function ExpenseForm(props) {
     };
 
     props.onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('');
-    setEnteredDate('');
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+  let expenseForm;
+  if (isFormDisplayed === true) {
+    expenseForm = (
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enteredAmount}
+              onChange={amountChnageHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              value={enteredDate}
+              onChange={dateChangeHandler}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
+        <div className="new-expense__actions">
+          <button type="reset" onClick={formDisplayHandler}>
+            Cancel
+          </button>
+          <button type="submit">Add Expense</button>
+        </div>
+      </form>
+    );
+  } else {
+    expenseForm = <button onClick={formDisplayHandler}>Add New Expense</button>;
+  }
 
-            onChange={amountChnageHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
-  );
+  return <div>{expenseForm}</div>;
 }
 
 export default ExpenseForm;
