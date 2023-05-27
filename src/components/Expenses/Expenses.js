97b9188d33caffb59394e34e95779fdcb37f3490
ReFowles <rebekah.fowles@gmail.com>
2023-05-27 17:01:52 -0600

    feat: add the chart component
    Completing Section 6.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const items = props.items;
@@ -24,6 +25,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangedYear={onChangedYearHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
